c9de0361c43a10dad0a7ca87589c0309b1bb6c69
Alejandro Pérez Martín <aleperzmartin@gmail.com>
2022-05-14 23:18:53 +0100

    Se añaden los test de buscador. closed #42

--- logica/src/test/java/es/iespuertodelacruz/gppuerto/model/BuscadorTest.java
+++ logica/src/test/java/es/iespuertodelacruz/gppuerto/model/BuscadorTest.java
@@ -22,51 +22,72 @@ public class BuscadorTest {
 
     @Test
     public void iniciarSesion() {
-        Assert.assertFalse("El método no ha funcionado",buscador.iniciarSesion("", ""));
+        Assert.assertFalse("El método no ha funcionado", buscador.iniciarSesion("", ""));
     }
 
     @Test
     public void registrarseTest() {
-        Assert.assertTrue("El método no ha funcionado",buscador.registrarse("usuario", "1234"));
+        Assert.assertTrue("El método no ha funcionado", buscador.registrarse("usuario", "1234"));
     }
 
     @Test
     public void accederUsuariosTest() {
-        Assert.assertNotNull("El método no ha funcionado",buscador.accederUsuarios());
+        Assert.assertNotNull("El método no ha funcionado", buscador.accederUsuarios());
     }
 
     @Test
     public void escribirUsuariosTest() {
-        Assert.assertTrue("El método no ha funcionado",buscador.escribirUsuarios("Prueba usuarios"));
-     }
+        Assert.assertTrue("El método no ha funcionado", buscador.escribirUsuarios("Prueba usuarios"));
+    }
 
     @Test
     public void buscarPilotoTest() {
-        
+        Assert.assertNotNull(buscador.buscarPiloto("Fernando Alonso"));
+    }
+
+    @Test
+    public void buscarPilotoNotTest() {
+        Assert.assertNull(buscador.buscarPiloto("Juan Enrique"));
     }
 
     @Test
     public void buscarEquipoTest() {
+        Assert.assertNotNull(buscador.buscarPiloto("Alpine"));
+    }
 
+    @Test
+    public void buscarEquipoNotTest() {
+        Assert.assertNull(buscador.buscarEquipo("Citroen"));
     }
 
     @Test
     public void buscarTemporadaTest() {
+        Assert.assertNotNull(buscador.buscarTemporada(2022));
+    }
 
+    @Test
+    public void buscarTemporadaNotTest() {
+        Assert.assertNull(buscador.buscarTemporada(2030));
     }
 
     @Test
     public void buscarCircuitoTest() {
-
+        Assert.assertNotNull(buscador.buscarCircuito("Monza"));
     }
 
     @Test
-    public void buscarCampeonTest() {
-
+    public void buscarCircuitoNotTest() {
+        Assert.assertNull(buscador.buscarCircuito("Tenerife"));
     }
 
     @Test
     public void buscarVehiculoTest() {
+        Assert.assertNotNull(buscador.buscarVehiculo("W13"));
+    }
+
+    @Test
+    public void buscarVehiculoNotTest() {
+        Assert.assertNull(buscador.buscarCircuito("Clio"));
 
     }
 }
